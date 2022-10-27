39fb35ebded8ae8ec33fc6c71be885604bdd604e
Nova Boman <nova.boman@chasacademy.se>
2022-10-27 20:45:04 +0200

    Add LoginForm logic

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -1,14 +1,55 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-confusing-arrow */
+/* eslint-disable indent */
+/* eslint-disable object-curly-newline */
 import { Formik, Form, Field } from 'formik';
+import React, { Dispatch, SetStateAction } from 'react';
 import styles from './styles/Forms.module.css';
 
-const LoginForm = () => {
-  const initialValues = { username: '', pwd: '' };
+type LoginFormProps = {
+  setError: Dispatch<SetStateAction<string>>;
+};
+
+const submitLogin = async (values: object) => {
+  try {
+    const result = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values),
+    }).then((res) =>
+      res.status === 200
+        ? true
+        : res.status === 404 || res.status === 401 || res.status === 400
+        ? res.json()
+        : 'Login failed'
+    );
+    return result;
+  } catch (e: any) {
+    return console.error(e);
+  }
+};
+
+const LoginForm: React.FC<LoginFormProps> = ({ setError }) => {
+  const initialValues = { username: '', password: '' };
+
+  const handleSubmitLogin = async (values: object) => {
+    const result = await submitLogin(values);
+    if (typeof result === 'string') {
+      setError(result);
+    }
+    if (typeof result === 'boolean') {
+      setError('');
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Formik
         initialValues={initialValues}
         onSubmit={(values, actions) => {
-          console.log(values);
+          handleSubmitLogin(values);
           actions.setSubmitting(false);
         }}
       >
@@ -20,8 +61,13 @@ const LoginForm = () => {
             name="username"
             placeholder="Username"
           />
-          <label htmlFor="pwd">Password</label>
-          <Field type="password" id="pwd" name="pwd" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+          <Field
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+          />
           <button type="submit">Login</button>
         </Form>
       </Formik>
