1cf54b739598b024f4d6fd272f6fe212f165d226
Nova Boman <nova.boman@chasacademy.se>
2022-10-28 17:21:56 +0200

    Create token, set cookie, create logout function

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable import/prefer-default-export */
 import { NextApiRequest, NextApiResponse } from 'next';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { serialize } from 'cookie';
 import UserModel from '../models/UserModel';
 import dbConnect from '../utils/mongoose.connect';
 
@@ -51,8 +54,47 @@ export const login = async (req: NextApiRequest, res: NextApiResponse) => {
     if (!validPassword) {
       return res.status(401).json('Invalid password');
     }
-    return res.status(200).json('Logged in');
-  } catch (e: any) {
+    if (user && validPassword) {
+      const token = jwt.sign(
+        { id: user._id, username: user.username, roles: user.roles },
+        process.env.JWT_SECRET!,
+        {
+          expiresIn: 60 * 15,
+        }
+      );
+
+      const cookie = serialize('skylogAccessToken', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 60 * 15,
+        path: '/',
+      });
+
+      res.setHeader('Set-Cookie', cookie);
+      return res.status(200).json({ user: user.username });
+    }
     return res.status(500).json('Could not sign in');
+  } catch (e: any) {
+    return res.status(500).json(e);
+  }
+};
+
+export const logout = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { cookies } = req;
+
+  const token = cookies.skylogAccessToken;
+
+  if (token) {
+    const emptyCookie = serialize('skylogAccessToken', '', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 1,
+      path: '/',
+    });
+
+    res.setHeader('Set-Cookie', emptyCookie);
+    res.status(200).json('Successfully logged out');
   }
 };
