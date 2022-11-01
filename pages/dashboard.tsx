f923612f6406914efd71e18c07a523bc6a1eb6f7
Nova Boman <nova.boman@chasacademy.se>
2022-11-01 22:01:29 +0100

    Create dashboard grid

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,17 +3,18 @@ import Head from 'next/head';
 import styles from '../styles/Dashboard.module.css';
 
 const Dashboard: NextPage = () => (
-  
+  <>
     <Head>
       <title>SkyLog</title>
       <meta name="description" content="Logbook for skydivers" />
     </Head>
-    <main className={styles.main}>
-      <div className="container">
-        <h1>Welcome to dashboard!</h1>
-      </div>
-    </main>
-  
+    <div className={styles.grid}>
+      <header className={styles.header}>Header</header>
+      <nav className={styles.nav}>Nav</nav>
+      <main className={styles.main}>Main</main>
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  </>
 );
 
 export default Dashboard;
