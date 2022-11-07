d4dd12391a5d3f87d9e26ac3321feeb06f8f7856
Nova Boman <nova.boman@chasacademy.se>
2022-11-07 23:17:58 +0100

    Update dashboard components and css for grid

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import DashboardMain from '../components/main/DashboardMain';
+import Header from '../components/header/Header';
+import MainNav from '../components/navigation/MainNav';
 import styles from '../styles/Dashboard.module.css';
 
 const Dashboard: NextPage = () => (
@@ -9,9 +12,9 @@ const Dashboard: NextPage = () => (
       <meta name="description" content="Logbook for skydivers" />
     </Head>
     <div className={styles.grid}>
-      <header className={styles.header}></header>
-      <nav className={styles.nav}></nav>
-      <main className={styles.main}></main>
+      <Header className={styles.header} heading={'Dashboard'} />
+      <MainNav className={styles.nav} />
+      <DashboardMain className={styles.main} />
       <footer className={styles.footer}></footer>
     </div>
   </>
