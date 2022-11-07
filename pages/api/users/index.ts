ff05b63871997db233f667036e724c2c13dd922f
Nova Boman <nova.boman@chasacademy.se>
2022-11-08 00:29:35 +0100

    Create API route for getting all users

---
+++ pages/api/users/index.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getAllUsers } from '../../../controllers/users';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    try {
+      return await getAllUsers(res);
+    } catch (e: any) {
+      return res.status(500).json("Couldn't process request");
+    }
+  } else {
+    return res.status(400).json('Request method must be GET');
+  }
+}
