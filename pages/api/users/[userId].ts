0505955d47af4c5292604237fa3db8738e234f2b
Nova Boman <nova.boman@chasacademy.se>
2022-11-10 01:11:12 +0100

    Create dynamic route for user/[userId]

---
+++ pages/api/users/[userId].ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { updateUserById } from '../../../controllers/users';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Get user by Id
+  if (req.method === 'GET') {
+    try {
+      return res.status(200).json('GET');
+    } catch (e: any) {
+      return res.status(500).json("Couldn't process request");
+    }
+  }
+  // Update user by Id
+  if (req.method === 'PATCH') {
+    try {
+      return await updateUserById(req, res);
+    } catch (e: any) {
+      return res.status(500).json("Couldn't process request");
+    }
+  }
+  // Delete user by Id
+  if (req.method === 'DELETE') {
+    return res.status(200).json('DELETE');
+  }
+  return res.status(400).json('Request method must be PATCH');
+}
