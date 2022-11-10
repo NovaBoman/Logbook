94ef4d188889d5e47735b64e7382bb208468f7d2
Nova Boman <nova.boman@chasacademy.se>
2022-11-10 01:10:11 +0100

    Add updateUserById function to controller

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,13 +1,35 @@
-import { NextApiResponse } from 'next';
-import UserModel from '../models/UserModel';
+import { NextApiRequest, NextApiResponse } from 'next';
+import UserModel, { IUser, IUserFields } from '../models/UserModel';
 import dbConnect from '../utils/mongoose.connect';
 
 export const getAllUsers = async (res: NextApiResponse) => {
   try {
     await dbConnect();
-    const users = await UserModel.find({});
+    const users: IUser[] = await UserModel.find({});
     return res.status(200).json(users);
   } catch (e: any) {
     return res.status(500).json(e);
+  }
+};
+
+export const updateUserById = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const fields: IUserFields = { ...req.body };
+  const options = { returnDocument: 'after' };
+  try {
+    await dbConnect();
+    const updatedUser = await UserModel.findOneAndUpdate(
+      { _id: req.query.userId },
+      fields,
+      options
+    );
+    if (!updatedUser) {
+      return res.status(404).json('No User with this Id');
+    }
+    return res.status(201).json(updatedUser);
+  } catch (e) {
+    return res.status(500).json('Could not update user');
   }
 };
