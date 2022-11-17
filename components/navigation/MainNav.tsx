181676df460b76b8d6a1e48894f304f868a30550
Nova Boman <nova.boman@chasacademy.se>
2022-11-17 20:57:43 +0100

    Update MainNav to use Link component and

--- components/navigation/MainNav.tsx
+++ components/navigation/MainNav.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { INav } from '../component.interfaces';
 import styles from './styles/Nav.module.css';
@@ -7,7 +8,16 @@ const MainNav: React.FC<INav> = ({ className }) => (
     <section>
       <h2>Admin</h2>
       <ul>
-        <li>Manage users</li>
+        <li>
+          <Link
+            href={{
+              pathname: '/dashboard',
+            }}
+            as={'dashboard/users'}
+          >
+            Manage users
+          </Link>
+        </li>
       </ul>
     </section>
 
@@ -15,7 +25,15 @@ const MainNav: React.FC<INav> = ({ className }) => (
       <h2>Profile</h2>
       <ul>
         <li>
-          Logs
+          <Link
+            href={{
+              pathname: '/dashboard',
+            }}
+            as={'dashboard/logs'}
+          >
+            Logs
+          </Link>
+
           <ul>
             <li>2022</li>
             <li>2021</li>
