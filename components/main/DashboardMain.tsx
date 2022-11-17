b6154218dd5209e86c72b449cfe8aeb4e98a0690
Nova Boman <nova.boman@chasacademy.se>
2022-11-17 20:59:02 +0100

    Create hook to render content in DashboardMain depending on route

--- components/main/DashboardMain.tsx
+++ components/main/DashboardMain.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable arrow-body-style */
+import { useRouter } from 'next/router';
 import React from 'react';
 import { IMain } from '../component.interfaces';
-import UserList from '../users/UserList';
+import { useContentSwitch } from './useContentSwitch';
 
-const DashboardMain: React.FC<IMain> = ({ className }) => (
-  <div className={className}>
-    <UserList />
-  </div>
-);
+const DashboardMain: React.FC<IMain> = ({ className }) => {
+  const router = useRouter();
+  const url = router.asPath;
+
+  // useContentSwitch returns a component depending on the current URL.
+  return <main className={className}>{useContentSwitch(url)}</main>;
+};
+
 export default DashboardMain;
