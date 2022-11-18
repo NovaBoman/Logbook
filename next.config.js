4818d770e9c983af1b02ec06501d9f7e75fb8b9c
Nova Boman <nova.boman@chasacademy.se>
2022-11-18 14:37:10 +0100

    Add redirects to avoid 404 on page reload

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/dashboard/users',
+        destination: '/dashboard',
+        permanent: true,
+      },
+      {
+        source: '/dashboard/logs',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
