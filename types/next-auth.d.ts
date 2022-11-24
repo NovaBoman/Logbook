74bd0e698a8d02aab3093d09c192ff2e2b90d2a5
Nova Boman <nova.boman@chasacademy.se>
2022-11-24 12:54:32 +0100

    Augment Session interface

---
+++ types/next-auth.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      name?: string | null | undefined;
+      roles?: Array<string> | null | undefined;
+    };
+  }
+}
