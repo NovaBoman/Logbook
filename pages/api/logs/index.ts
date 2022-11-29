d6d62df6f1af90419ed4f9c4d42ce85c7b0608b2
Nova Boman <nova.boman@chasacademy.se>
2022-11-29 12:55:22 +0100

    Move getUserLogs to /logs

--- pages/api/logs/index.ts
+++ pages/api/logs/index.ts
@@ -1,12 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getToken } from 'next-auth/jwt';
-import { createLog } from '../../../services/log.service';
+import { createLog, getUserLogs } from '../../../services/log.service';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const token = await getToken({ req });
+  if (req.method === 'GET') {
+    try {
+      if (!token || !token?.user.name) {
+        return res.status(401).json('Unauthorized');
+      }
+      return await getUserLogs(req, res, token.user.name);
+    } catch (e: any) {
+      return res.status(500).json("Couldn't process request");
+    }
+  }
   if (req.method === 'POST') {
     try {
       if (!token || !token?.user) {
